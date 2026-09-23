b40a969bbebeb38403b73af8d7c82a09420b1178
nadeeshapriyamal2003-del <nadeeshapriyamal2003@gmail.com>
2026-09-23 11:38:20 +0530

    initialized resources and styled it

---
+++ src/components/sections/guidance-resources/HowItWorks.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const Guidence = () => {
+  return (
+    <div className="howitworks">
+      <div className="top">
+        <div className="left">How IEEE works</div>
+        <div className="right">// 2.0</div>
+      </div>
+      <div className="content">
+        <div className="text-box">
+          <h1>FORM YOUR</h1>
+          <h1 className="orange">TEAM.</h1>
+
+          <div className="subtext">
+            <p className="text">
+              Bring IEEE student members and prepare to compete.
+            </p>
+          </div>
+        </div>
+
+        <div className="text-box">
+          <h1 className="orange">24 HOURS,</h1>
+          <h1>ONE CHALLENGE</h1>
+          <div className="subtext">
+            <p className="text">Solve Programming problems against the clock</p>
+          </div>
+        </div>
+
+        <div className="text-box">
+          <h1>TURN PROBLEMS</h1>
+          <h1 className="orange">
+            <span>INTO </span>PROBLEM.
+          </h1>
+          <div className="subtext">
+            <p className="text">
+              Take the challenge , develop an algorithm and build your solution.
+            </p>
+          </div>
+        </div>
+
+        <div className="text-box">
+          <h1>
+            WRITE , TEST ,<span className="orange">REFINE.</span>
+          </h1>
+          <div className="subtext">
+            <p className="text">
+              Check your logic , fix errors , and submit your solutions.
+            </p>
+          </div>
+        </div>
+
+        <div className="text-box">
+          <h1>COMPETE WITH</h1>
+          <h1 className="orange">THE WORLD.</h1>
+          <div className="subtext">
+            <p className="text">
+              Put your Teamwork and problem solving skills to test globally.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Guidence;
